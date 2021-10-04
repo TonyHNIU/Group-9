f78e993ea5d3e6ed8dacad3b72a83f742a6f23a0
HUI NIU <hui-niu@outlook.com>
2021-10-05 12:01:06 +1300

    try to display the selected activities

--- client/src/components/Goal/Table/Activities.js
+++ client/src/components/Goal/Table/Activities.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { Dialog, DialogTitle, Button, Typography } from '@material-ui/core'
 import * as AiIcons from 'react-icons/ai'
 import Clone2 from '../Dnd/Board'
+import Card from '../Dnd/Card'
 
 const Activities = (props) => {
     const { openPopup, setOpenPopup } = props
@@ -21,7 +22,14 @@ const Activities = (props) => {
                     {
                         acts.map(act => (
                             <label>
-                                
+                                <Card
+                                id='card-1'
+                                className='card'
+                                draggable='true'
+                                >
+                                    {act}
+                                </Card>
+                                <br />
                             </label>
                         ))
                     }
