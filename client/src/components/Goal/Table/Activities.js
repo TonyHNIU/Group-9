9e97bace1629081cc4457fb1eb5b2fe2eef1f638
HUI NIU <hui-niu@outlook.com>
2021-10-05 11:23:19 +1300

    set up pop up function

--- client/src/components/Goal/Table/Activities.js
+++ client/src/components/Goal/Table/Activities.js
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Dialog, DialogTitle, Button, Typography } from '@material-ui/core'
 import * as AiIcons from 'react-icons/ai'
+import Clone2 from '../Dnd/Board'
 
 const Activities = (props) => {
+    const { openPopup, setOpenPopup } = props
+
     return (
         <Dialog maxWidth='md' fullScreen>
             <DialogTitle>
                 <div>
                     <Typography style={{ flexGrow: 1 }}>Collected Activities here</Typography>
-                    <Button color='secondary'><AiIcons.AiOutlineClose /></Button>
+                    <Button color='secondary' onClick={() => { setOpenPopup(false) }}><AiIcons.AiOutlineClose /></Button>
                 </div>
             </DialogTitle>
             <div>
-                
+                <Clone2 id='board-1' className='board'>
+
+                </Clone2>
             </div>
         </Dialog>
     )
