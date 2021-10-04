37608289f3ee027434a9ee95fbe078aba325f22d
Kelvin Wong <kakoo40@gmail.com>
2021-10-05 00:25:01 +1300

    Progress on activities output
    for each bug still needs to be fixed

--- client/src/components/Goals/GoalTable.js
+++ client/src/components/Goals/GoalTable.js
@@ -1,26 +1,47 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
-import tablecolumns from './columns.js';
-import goaldata from './goaldata.js';
+import tableColumns from './columns.js';
+import goalData from './goaldata.js';
+
+const {data} = {
+    data: {columns:{tableColumns},
+        rows: {goalData}}
+  }
 
 export default function GoalTable() {
-    const [select, setSelection] = React.useState([]);
+    let activitiesList = '';
     return (
         <div style={{ height: 700, width: '100%' }}>
             <DataGrid
-                rows={goaldata}
-                columns={tablecolumns}
+                rows={goalData}
+                columns={tableColumns}
                 pageSize={25}
                 rowsPerPageOptions={[25]}
                 checkboxSelection
                 hideFooterPagination
-                onSelectionModelChange={(newSelection) => {
-                    setSelection(newSelection.goaldata);
-                    console.log(newSelection);
+                onSelectionModelChange={(ids) => {
+                    activitiesList = '';
+                    const selectedIDs = new Set(ids);
+                    const selectedRowData = data.rows.goalData.filter((row) =>
+                      selectedIDs.has(row.id)
+                    );
+                    selectedRowData.forEach(goal => 
+                        goal.VeryHighContribution.forEach(activity => 
+                            activity.VeryHighContribution.forEach(vhActivity =>
+                                {if (!activitiesList.includes(vhActivity)){
+                                    activitiesList += (vhActivity, ' ');
+                                }}),
+                                activity.HighContribution.forEach(hActivity =>
+                                    {if (!activitiesList.includes(hActivity)){
+                                        activitiesList += (hActivity, ' ');
+                                    }})
+                            )
+                        )
                     }
-                }
+                }       
             />
+            <p>{activitiesList}</p>
         </div>
     );
 }
