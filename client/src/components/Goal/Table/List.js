2523d7a32e68b9ae37f60f500c49da14c8e67923
HUI NIU <hui-niu@outlook.com>
2021-10-04 18:08:13 +1300

    refactoring the goal page

--- client/src/components/Goal/Table/List.js
+++ client/src/components/Goal/Table/List.js
@@ -1,32 +1,15 @@
-import React from 'react'
-import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Typography, CircularProgress, Grid } from '@material-ui/core'
+import React, { useState } from 'react'
 
 import { useSelector } from 'react-redux'
-import SubList from './SubList'
 
-const List = () => {
+const Goal = () => {
   const goals = useSelector((state) => state.goals)
 
   return (
     <>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography align='center' variant='h6'>Goals</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {!goals.length
-              ? <CircularProgress />
-              : (<Grid>{goals.map((goal) => (<Grid key={goal}><SubList goal={goal} /></Grid>))}</Grid>)}
-          </TableBody>
-        </Table>
-      </TableContainer>
+     
     </>
   )
 }
 
-export default List
+export default Goal
