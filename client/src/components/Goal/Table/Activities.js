d121c799434f5c3eecaa5b1b90f3abb378d5963b
HUI NIU <hui-niu@outlook.com>
2021-10-04 18:30:08 +1300

    building activities dialog

---
+++ client/src/components/Goal/Table/Activities.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Dialog, DialogTitle, Button, Typography } from '@material-ui/core'
+
+const Activities = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Activities
