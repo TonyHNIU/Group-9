e5cb2384eefe284cb2c90b61126e8d566478178a
HUI NIU <hui-niu@outlook.com>
2021-10-04 18:34:38 +1300

    inital dialog freamwork

--- client/src/components/Goal/Table/Activities.js
+++ client/src/components/Goal/Table/Activities.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Dialog, DialogTitle, Button, Typography } from '@material-ui/core'
+import * as AiIcons from 'react-icons/ai'
 
-const Activities = () => {
+const Activities = (props) => {
     return (
-        <div>
-            
-        </div>
+        <Dialog maxWidth='md' fullScreen>
+            <DialogTitle>
+                <div>
+                    <Typography style={{ flexGrow: 1 }}>Collected Activities here</Typography>
+                    <Button color='secondary'><AiIcons.AiOutlineClose /></Button>
+                </div>
+            </DialogTitle>
+            <div>
+                
+            </div>
+        </Dialog>
     )
 }
 
