e1dc9d88f2fa908c1261f26cbce86fccdb88adc9
HUI NIU <hui-niu@outlook.com>
2021-10-04 19:21:58 +1300

    testing reset button

--- client/src/components/Goal/Table/List.test.js
+++ client/src/components/Goal/Table/List.test.js
@@ -6,3 +6,7 @@ import { render } from '@testing-library/react'
 it("try rendering function", () => {
     const {getBytestId} = render(<button label='click me' type="submit">Submit</button>)
 })
+
+it("rendering reset function", () => {
+    const resetGoals = render(<button label='reset goals'>Reset</button>)
+})
