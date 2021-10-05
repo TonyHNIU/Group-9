d0ef734012f14ece35d7bc5b993f88bc4fba82dc
HUI NIU <hui-niu@outlook.com>
2021-10-06 09:25:39 +1300

    Fixing JavaScript Standard Style

--- client/src/components/Goal/Table/Activities.js
+++ client/src/components/Goal/Table/Activities.js
@@ -6,76 +6,76 @@ import Clone2 from '../Dnd/Board'
 import Card from '../Dnd/Card'
 
 const Activities = (props) => {
-    const { openPopup, setOpenPopup } = props
+  const { openPopup, setOpenPopup } = props
 
-    const acts = props.sums;
-    return (
-        <Dialog open={openPopup} maxWidth='md' fullScreen>
-            <DialogTitle>
-                <div>
-                    <Typography style={{ flexGrow: 1 }}>Collected Activities here</Typography>
-                    <Button color='secondary' onClick={() => { setOpenPopup(false) }}><AiIcons.AiOutlineClose /></Button>
-                </div>
-            </DialogTitle>
-            <div>
-                <Clone2 id='board-1' className='board'>
-                    {
+  const acts = props.sums
+  return (
+    <Dialog open={openPopup} maxWidth='md' fullScreen>
+      <DialogTitle>
+        <div style={{ display: 'flex' }}>
+          <Typography style={{ flexGrow: 1 }}>Collected Activities here</Typography>
+          <Button color='secondary' onClick={() => { setOpenPopup(false) }}><AiIcons.AiOutlineClose /></Button>
+        </div>
+      </DialogTitle>
+      <div>
+        <Clone2 id='board-1' className='board'>
+          {
                         acts.map(act => (
-                            <label>
-                                <Card
-                                id='card-1'
-                                className='card'
-                                draggable='true'
-                                >
-                                    {act}
-                                </Card>
-                                <br />
-                            </label>
+                          <label>
+                            <Card
+                              id='card-1'
+                              className='card'
+                              draggable='true'
+                            >
+                              {act}
+                            </Card>
+                            <br />
+                          </label>
                         ))
                     }
-                </Clone2>
-                <p>Week 1 - 2</p>
-                <Clone2>
-                    <label>
-                        <Card
-                        id='card-2'
-                        className='card'
-                        draggable='true'
-                        ></Card>
-                        <br />
-                        <br />
-                        <br />
-                    </label>
-                </Clone2>
-                <p>Week 3 - 4</p>
-                <Clone2>
-                    <label>
-                        <Card
-                        id='card-2'
-                        className='card'
-                        draggable='true'
-                        ></Card>
-                        <br />
-                        <br />
-                        <br />
-                    </label>
-                </Clone2>
-                <p>Week 5 - 6</p>
-                <Clone2>
-                    <label>
-                        <Card
-                        id='card-2'
-                        className='card'
-                        draggable='true'
-                        ></Card>
-                        <br />
-                        <br />
-                        <br />
-                    </label>
-                </Clone2>
-            </div>
-        </Dialog>
-    )
+        </Clone2>
+        <p>Week 1 - 2</p>
+        <Clone2>
+          <label>
+            <Card
+              id='card-2'
+              className='card'
+              draggable='true'
+            />
+            <br />
+            <br />
+            <br />
+          </label>
+        </Clone2>
+        <p>Week 3 - 4</p>
+        <Clone2>
+          <label>
+            <Card
+              id='card-2'
+              className='card'
+              draggable='true'
+            />
+            <br />
+            <br />
+            <br />
+          </label>
+        </Clone2>
+        <p>Week 5 - 6</p>
+        <Clone2>
+          <label>
+            <Card
+              id='card-2'
+              className='card'
+              draggable='true'
+            />
+            <br />
+            <br />
+            <br />
+          </label>
+        </Clone2>
+      </div>
+    </Dialog>
+  )
 }
 
 export default Activities
--- client/src/components/Goal/Table/List.js
+++ client/src/components/Goal/Table/List.js
@@ -1,57 +1,57 @@
 import React, { useState } from 'react'
 
 import { useSelector } from 'react-redux'
-import { Form, Field, useField } from 'react-final-form';
-import arrayMutators from 'final-form-arrays';
-import { Table, List } from '@material-ui/core';
+import { Form, Field, useField } from 'react-final-form'
+import arrayMutators from 'final-form-arrays'
+import { Table, List } from '@material-ui/core'
 import Activities from './Activities'
 
 const Goal = () => {
   const goals = useSelector((state) => state.goals)
 
-  const [selecteds,setSelecteds] = useState([]);
-  
+  const [selecteds, setSelecteds] = useState([])
+
   const onSubmit = values => {
     if (values === 'null') {
-      return selecteds;
+      return selecteds
     } else {
-      setSelecteds(values);
+      setSelecteds(values)
     }
-};
+  }
 
-const mutiVeryHighArray = selecteds.data?.map((item) => {
-  return item.veryHigh;
-});
-const veryHigh = [...new Set(mutiVeryHighArray?.flat())];
+  const mutiVeryHighArray = selecteds.data?.map((item) => {
+    return item.veryHigh
+  })
+  const veryHigh = [...new Set(mutiVeryHighArray?.flat())]
 
-const mutiHighArray = selecteds.data?.map((item) => {
-  return item.high;
-});
-const high = [...new Set(mutiHighArray?.flat())];
+  const mutiHighArray = selecteds.data?.map((item) => {
+    return item.high
+  })
+  const high = [...new Set(mutiHighArray?.flat())]
 
-const sums = [...new Set(veryHigh.concat(high))];
+  const sums = [...new Set(veryHigh.concat(high))]
 
-const [openPopup, setOpenPopup] = useState(false)
+  const [openPopup, setOpenPopup] = useState(false)
 
   return (
     <div>
       <h3>Goals</h3>
       <Form
-      onSubmit={onSubmit}
-      mutators={{
+        onSubmit={onSubmit}
+        mutators={{
           ...arrayMutators
         }}
         render={({ handleSubmit, form, submitting, pristine, values }) => (
           <form onSubmit={handleSubmit}>
             <label>Select any goals you want</label>
-            {/*<pre>{JSON.stringify(options, 0, 2)}</pre>*/}
+            {/* <pre>{JSON.stringify(options, 0, 2)}</pre> */}
             <div>
               {goals.map(goal => (
                 <label key='{sum}'>
                   <Field
-                    name="data"
-                    component="input"
-                    type="checkbox"
+                    name='data'
+                    component='input'
+                    type='checkbox'
                     value={goal}
                     key='{sum}'
                   />
@@ -62,18 +62,18 @@ const [openPopup, setOpenPopup] = useState(false)
             </div>
             <hr />
             <div>
-              <button type="submit" disabled={submitting || pristine}>
+              <button type='submit' disabled={submitting || pristine}>
                 Submit
               </button>
               <button
-                type="button"
+                type='button'
                 onClick={form.reset}
                 disabled={submitting || pristine}
               >
                 Reset
               </button>
               <button
-              onClick={() => setOpenPopup(true)}
+                onClick={() => setOpenPopup(true)}
               >
                 Popup
               </button>
